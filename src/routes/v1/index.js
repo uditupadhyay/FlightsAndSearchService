bb3ddda7f77e7adec952210ccea59471a13afe69
uditmar24 <uditmar24@gmail.com>
2023-03-15 22:03:43 +0530

    added middleware and errorcodes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+
+const {FlightMiddlewares } =require("../../middlewares/index");
+
+
 const CityController = require('../../controllers/city-controller');
 const FlightController = require('../../controllers/flight-controller');
 const AirportController=require('../../controllers/airport-controller');
@@ -12,7 +16,9 @@ router.get('/city/:id',CityController.get);
 router.get('/city',CityController.getAll);
 router.patch('/city/:id',CityController.update);
 
-router.post('/flights',FlightController.create);
+router.post('/flights',
+FlightMiddlewares.validateCreateFlight,
+FlightController.create);
 router.get('/flights',FlightController.getAll);
 
 router.post('/airports',AirportController.create);
